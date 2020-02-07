ab0d16e851101da5745220aebd33215dc92155e5
jo <ljonas@riseup.net>
2020-02-08 00:36:44 +0100

    Remove 'GSD_CMS' prepended message from logger

--- src/utils.ts
+++ src/utils.ts
@@ -4,11 +4,11 @@ Colors.black;
 
 export const log = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  info: (msg: string, ...args: any[]): void => console.log('GSD_CMS'.blue, 'info'.cyan, msg, ...args),
+  info: (msg: string, ...args: any[]): void => console.log('info'.cyan, msg, ...args),
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  warn: (msg: string, ...args: any[]): void => console.log('GSD_CMS'.blue, 'warning'.yellow, msg, ...args),
+  warn: (msg: string, ...args: any[]): void => console.log('warning'.yellow, msg, ...args),
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  error: (msg: string, ...args: any[]): void => console.error('GSD_CMS'.blue, 'error'.red, msg, ...args),
+  error: (msg: string, ...args: any[]): void => console.error('error'.red, msg, ...args),
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  success: (msg: string, ...args: any[]): void => console.log('GSD_CMS'.blue, 'success'.green, msg, ...args),
+  success: (msg: string, ...args: any[]): void => console.log('success'.green, msg, ...args),
 };
