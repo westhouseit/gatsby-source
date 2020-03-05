a70c3b26f9e5a9c6ec13161ba51ef6fd1e025789
jo <ljonas@riseup.net>
2020-03-05 18:52:37 +0100

    Split unit testing and e2e testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['src', 'tests'],
+  roots: ['src', 'tests', 'e2e'],
   testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
   transform: {
     '^.+\\.(ts)$': 'ts-jest',
